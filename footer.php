56c64e5b7c6ac4eaee50dded6e41a6b9b6d68bbf
Emily Schleiner <cordial.emily@gmail.com>
2017-07-31 20:16:52 -0700

    footer info update

--- footer.php
+++ footer.php
@@ -11,10 +11,10 @@
                 <h3>Get to know us</h3>
                 <br>
                 <ul class="nav nav-pills nav-stacked social">
-                  <li><p><a href="https://twitter.com/cordial_emily"><span class="dashicons dashicons-twitter"></span></a>
-                         <a href="https://www.facebook.com/CordialEmily"><span class="dashicons dashicons-facebook"></span></a>
-                         <a href="https://knownhabits.wordpress.com/"><span class="dashicons dashicons-rss"></span></a>
-                         <a href="https://plus.google.com/u/0/111928960329290719990"><span class="dashicons dashicons-googleplus"></span></a></p>
+                  <li><p><a href="https://twitter.com/ContactParallel"><span class="dashicons dashicons-twitter"></span></a>
+                         <a href="https://www.facebook.com/mindfulcomputing/"><span class="dashicons dashicons-facebook"></span></a>
+                         <a href=""><span class="dashicons dashicons-rss"></span></a>
+                         <a href=""><span class="dashicons dashicons-googleplus"></span></a></p>
                 </ul>
                 <br>
                 </br>
@@ -37,8 +37,8 @@
               </br>
               <h3>Contact us</h3>
               <ul class="nav nav-pills nav-stacked social">
-                <li><p><a href="mailto:cordial.emily@gmail.com?Subject=Hello%20ParallelDesign" target="_top"><span class="dashicons dashicons-email-alt"></span></a></p></li>
-                <li><p><a href="https://twitter.com/cordial_emily"></a></p></li>
+                <li><p><a href="mailto:contact@paralleldesign.net?Subject=Parallel%Design%Contact:%Hello!" target="_top"><span class="dashicons dashicons-email-alt"></span></a></p></li>
+                <li><p><a href="https://twitter.com/ContactParallel"></a></p></li>
               </ul>
               <br>
               </br>
